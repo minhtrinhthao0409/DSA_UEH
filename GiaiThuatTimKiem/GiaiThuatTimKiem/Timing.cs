6130ed5a0aef0fdf96cbcfa9f48668bb0750349d
trinhthaominh <92960404+trinhthaominh@users.noreply.github.com>
2025-03-06 15:01:45 +0700

    add Timing class

---
+++ GiaiThuatTimKiem/GiaiThuatTimKiem/Timing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GiaiThuatTimKiem
+{
+    public class Timing
+    {
+        TimeSpan startingTime;
+        TimeSpan duration;
+        public Timing()
+        {
+            startingTime = new TimeSpan(0);
+            duration = new TimeSpan(0);
+        }
+        public void StopTime()
+        {
+            duration =
+            Process.GetCurrentProcess().Threads[0].
+            UserProcessorTime.
+            Subtract(startingTime);
+        }
+        public void startTime()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            startingTime =
+            Process.GetCurrentProcess().Threads[0].
+            UserProcessorTime;
+        }
+        public TimeSpan Result()
+        {
+            return duration;
+        }
+    }
+}
